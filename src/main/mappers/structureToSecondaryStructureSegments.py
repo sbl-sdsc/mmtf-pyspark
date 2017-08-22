4787c004e1a158779c7aa1198d75921815a9498a
marshuang80 <marshuang80@gmail.com>
2017-08-22 16:33:00 -0700

    finisehd structureToSecondaryStructureSegments, still need testing

---
+++ src/main/mappers/structureToSecondaryStructureSegments.py
@@ -0,0 +1,50 @@
+#!/user/bin/env python
+'''
+structureToSecondaryStructureSegments.py:
+
+Authorship information:
+    __author__ = "Yue Yu"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmai.com:
+    __status__ = "debug"
+
+'''
+from pyspark.sql import Row
+
+class structureToSecondaryStructureSegments(object):
+    '''Maps chain sequences to it's sequence segments.
+    The length must be an odd number
+
+    Attributes:
+    '''
+    def __init__(self, length):
+        '''Constructor sets the segment length.
+
+        '''
+        if length % 2 != 1:
+            print("length has to be an odd number")
+
+        self.length = length
+
+
+    def __call__(self, t):
+        # TODO double check indexing
+        structureChainId = t[0]
+        sequence = t[1]
+        dsspQ3 = t[6]
+        dsspQ8 = t[5]
+
+        numSegments = max(0, len(sequence) - self.length)
+        sequences = []
+
+        for i in range(len(sequence)):
+            currSeq = sequence[i:i+self.length]
+            labelQ3 = dsspQ3[i+ int(self.length/2), i + int(self.length/2) + 1]
+            labelQ8 = dsspQ8[i+ int(self.length/2), i + int(self.length/2) + 1]
+
+            if (labelQ8 != "X" and labelQ3 != "X"):
+                sequences.append(Row(structureChainId, currSeq, labelQ8, labelQ3))
+
+        return sequences
+
+
