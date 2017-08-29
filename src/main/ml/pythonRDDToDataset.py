6bc99c0c527e24fd338a53bd3ac84b47247d2009
marshuang80 <marshuang80@gmail.com>
2017-08-29 11:56:20 -0700

    finished implementing pythonRDDToDataSet

---
+++ src/main/ml/pythonRDDToDataset.py
@@ -0,0 +1,45 @@
+#!/user/bin/env python
+'''
+pythonRDDToDataset.py:
+Authorship information:
+    __author__ = "Peter Rose"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmai.com:
+    __status__ = "debug"
+'''
+from pyspark.sql.types import *
+from pyspark.sql import SparkSession
+
+# TODO colNames to be string or list
+def getDataset(data, colNames):
+	row = data.first()
+    length = len(row)
+
+    if length != len(colNames):
+        raise Exception("colNames length does not match row length")
+
+    sf = []
+
+    for i in range(colNames):
+        o = row[i]
+        if type(o) == str:
+            sf.append(StructField(colNames[i], StringType(), False))
+        elif type(o) == int:
+            sf.append(StructField(colNames[i], IntegerType(), False))
+        elif type(o) == float:
+            sf.append(StructField(colNames[i], FloatType(), False))
+        elif type(o) == long:
+            sf.append(StructField(colNames[i], LongType(), False))
+        else:
+            print("Data type not implemented yet")
+
+    schema = StuctType(sf)
+    spark = SparkSession.builder.getOrCreate()
+    return spark.createDataFrame(data, schema)
+
+
+
+
+
+
+
