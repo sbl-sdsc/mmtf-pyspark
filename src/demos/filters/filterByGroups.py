96dd6a17e79e90715714ae4ec697dfbb74c766c7
marshuang80 <marshuang80@gmail.com>
2017-09-06 14:38:32 -0700

    More Demos

---
+++ src/demos/filters/filterByGroups.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+'''
+filterByGroups.py
+
+This example demonstrates how to filter structures with
+specified groups (residues).
+Groups are specified by their one, two or three letter codes
+e.g. "F", "MG", "ATP", as defined in the
+
+<a href="https://www.wwpdb.org/data/ccd">wwPDB Chemical Component Dictionary</a>.
+
+Authorship information:
+    __author__ = "Mars Huang"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmail.com:
+    __status__ = "Done"
+'''
+
+from pyspark import SparkConf, SparkContext
+from src.main.io import MmtfReader
+from src.main.filters import containsGroup
+
+def main():
+	path = "/home/marshuang80/PDB/reduced"
+
+	conf = SparkConf().setMaster("local[*]") \
+                      .setAppName("FilterByGroup")
+	sc = SparkContext(conf = conf)
+
+	count = MmtfReader.readSequenceFile(path, sc) \
+                      .filter(containsGroup("ATP", "MG")) \
+                      .count()
+
+	print("Number of structure with ATP + MG : " +
+		str(count))
+
+	sc.stop()
+
+if __name__ == "__main__":
+	main()
