2769ec413bc376af97937767be275c6ed530484f
marshuang80 <marshuang80@gmail.com>
2017-09-14 12:56:50 -0700

    updated structure

--- mmtfPyspark/src/main/io/mmtf_structure.py
+++ mmtfPyspark/src/main/io/mmtf_structure.py
@@ -5,21 +5,8 @@ import struct
 
 class mmtf_structure(object):
     def run_length_decoder_numpy(self,in_array):
-        '''
-
-        switch=False
-        out_array=[]
-        in_array = in_array.tolist()
-        for item in in_array:
-            if switch==False:
-                this_item = item
-                switch=True
-            else:
-                switch=False
-                out_array.extend([this_item]*int(item))
-        return np.asarray(out_array, dtype=np.int32)
-
-        '''
+        """
+        """
         lengths = np.array(in_array[1::2])
         values = np.array(in_array[0::2])
         starts = np.insert(np.array([0]),1,np.cumsum(lengths))[:-1]
@@ -31,7 +18,6 @@ class mmtf_structure(object):
         return x
 
 
-
     def recursive_index_decode(self, int_array, max=32767, min=-32768):
         """Unpack an array of integers using recursive indexing.
         :param int_array: the input array of integers
@@ -40,83 +26,60 @@ class mmtf_structure(object):
         :return the array of integers after recursive index decoding"""
 
         out_arr = np.cumsum(int_array)/1000
-
         return out_arr[(int_array != max) & (int_array != min)]
-        '''
 
-        n = len(int_array) - (np.count_nonzero(int_array == 32767) + np.count_nonzero(int_array == -32768)) + 1
-        out_arr = np.full(n, np.nan)
-        count = 0
-        decoded_val = 0
-        for item in int_array.tolist():
-            if item==max or item==min:
-                decoded_val += item
+
+    def decode_entity_list(self, input_data):
+        """Convert byte strings to strings in the entity list.
+        :param input_data the list of entities
+        :return the decoded entity list"""
+
+        out_data = []
+        for entry in input_data:
+            out_data.append(self.convert_entity(entry))
+        return out_data
+
+
+    def decode_group_list(self, input_data):
+        """Convert byte strings to strings in the group map.
+        :param input_data the list of groups
+        :return the decoded group list"""
+
+        out_data = []
+        for entry in input_data:
+            out_data.append(self.convert_group(entry))
+        return out_data
+
+
+    def convert_group(self, input_group):
+        """Convert an individual group from byte strings to regula strings.
+        :param input_group the input group
+        :return the decoded group"""
+        output_group = {}
+        for key in input_group:
+            if key in [b'elementList', b'atomNameList']:
+                output_group[key.decode('ascii')] = [x.decode('ascii')
+                                                     for x in input_group[key]]
+            elif key in [b'chemCompType', b'groupName', b'singleLetterCode']:
+                output_group[key.decode('ascii')] = input_group[key].decode('ascii')
             else:
-                decoded_val += item
-                out_arr[count] = decoded_val
-                count += 1
-                #out_arr.append(decoded_val)
-                decoded_val = 0
+                output_group[key.decode('ascii')] = input_group[key]
+        return output_group
 
 
-        return out_arr
-
-
-        out_arr = []
-        decoded_val = 0
-        for item in int_array.tolist():
-            if item==max or item==min:
-                decoded_val += item
+    def convert_entity(self, input_entity):
+        """Convert an individual entity from byte strings to regular strings
+        :param input_entity the entity to decode
+        :return the decoded entity"""
+        output_entity = {}
+        for key in input_entity:
+            if key in [b'description', b'type', b'sequence']:
+                output_entity[key.decode(
+                    'ascii')] = input_entity[key].decode('ascii')
             else:
-                decoded_val += item
-                out_arr.append(decoded_val)
-                decoded_val = 0
-        return numpy.asarray(out_arr,dtype=numpy.int32)
+                output_entity[key.decode('ascii')] = input_entity[key]
+        return output_entity
 
-        out_arr = np.full(n, np.nan)
-        buf, val, i = 0, 0, 0
-        for diff in int_array.tolist():
-            if diff == max or diff == min:
-                buf += diff
-            else:
-                val += (diff + buf)
-                out_arr[i] = val
-                buf = 0
-                i += 1
-        #out_arr = np.full(n,np.nan)
-        out_arr = []
-        decoded_val = 0
-        #count = 0
-        #while count_np < len(int_array):
-        #for i in range(n):
-        for item in int_array.tolist():
-            if item==max or item==min:
-            #if int_array[count_np] == max or int_array[count_np] == min:
-                decoded_val +=  item
-                #count += 1
-            else:
-                #decoded_val += item
-                #decoded_val = int_array[count_np]
-                #out_arr[count] = item + decoded_val
-                out_arr.append(item + decoded_val)
-                #count_np += 1
-                #count += 1
-                decoded_val = 0
-
-        return np.cumsum(np.asarray(out_arr, dtype = np.int32))/1000
-        #return np.cumsum(out_arr)/1000
-        '''
-
-
-
-
-    def run_length_decoder(self,l):
-        x = [0]
-        for v,l in zip(l[::2],l[1::2]):
-            x += [v]*l
-        return x[1:]
-
-    def set_coord_list(self):
         self.x_coord_list = np.cumsum(self.recursive_index_decode(self.x_coord_list))/1000
         self.y_coord_list = np.cumsum(self.recursive_index_decode(self.y_coord_list))/1000
         self.z_coord_list = np.cumsum(self.recursive_index_decode(self.z_coord_list))/1000
@@ -124,23 +87,13 @@ class mmtf_structure(object):
 
     def __init__(self, input_data):
 
-        #self.x_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'xCoordList'][12:],'>i2'), np.frombuffer(input_data[b'xCoordList'][4:8],'>i')[0])
-        #self.y_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'yCoordList'][12:],'>i2'), np.frombuffer(input_data[b'yCoordList'][4:8],'>i')[0])
-        #self.z_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'zCoordList'][12:],'>i2'), np.frombuffer(input_data[b'zCoordList'][4:8],'>i')[0])
-
-
         self.x_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'xCoordList'][12:],'>i2'))
         self.y_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'yCoordList'][12:],'>i2'))
         self.z_coord_list = self.recursive_index_decode(np.frombuffer(input_data[b'zCoordList'][12:],'>i2'))
-
-        #self.x_coord_list = np.cumsum(self.recursive_index_decode(np.frombuffer(input_data[b'xCoordList'][12:],'>i2')))/1000
-        #self.y_coord_list = np.cumsum(self.recursive_index_decode(np.frombuffer(input_data[b'yCoordList'][12:],'>i2')))/1000
-        #self.z_coord_list = np.cumsum(self.recursive_index_decode(np.frombuffer(input_data[b'zCoordList'][12:],'>i2')))/1000
         if b"bFactorList" in input_data:
             self.b_factor_list = self.recursive_index_decode(np.frombuffer(input_data[b'xCoordList'][12:],'>i2'))
         else:
             self.b_factor_list = []
-
         if b'resolution' in input_data:
             self.resolution = input_data[b'resolution']
         else:
@@ -167,10 +120,7 @@ class mmtf_structure(object):
             self.deposition_date = input_data[b"depositionDate"].decode()
         else:
             self.deposition_date = None
-        if b"experimentalMethods" in input_data:
-            self.experimental_methods = [x.decode() for x in input_data[b"experimentalMethods"]]
-        else:
-            self.experimental_methods = None
+
         if b"title" in input_data:
             self.title = input_data[b"title"]
         else:
@@ -203,10 +153,6 @@ class mmtf_structure(object):
             self.sec_struct_list = np.frombuffer(input_data[b"secStructList"][12:],'>i1')
         else:
             self.sec_struct_list = []
-        if b"chainNameList" in input_data:
-            self.chain_name_list = [chr(a) for a in input_data[b"chainNameList"][12:][::4]]
-        else:
-            self.chain_name_list = []
 
 
         self.num_bonds = input_data[b"numBonds"]
@@ -218,16 +164,16 @@ class mmtf_structure(object):
         self.groups_per_chain = input_data[b"groupsPerChain"]
 
 
-        self.chain_id_list = [chr(a) for a in input_data[b"chainIdList"][12:][::4]]
+
         self.group_type_list = np.frombuffer(input_data[b'groupTypeList'][12:],'>i4') # double check /1000
 
 
         # TODO dictionary in bytes
         if b"entityList" in input_data:
-            self.entity_list = input_data[b"entityList"]
+            self.entity_list = self.decode_entity_list(input_data[b"entityList"])
         else:
             self.entity_list = []
-        self.group_list = input_data[b"groupList"]
+        self.group_list = self.decode_group_list(input_data[b"groupList"])
 
 
         if b"occupancyList" in input_data:
@@ -235,12 +181,7 @@ class mmtf_structure(object):
         else:
             self.occupancy_list = []
 
-        if b"altLocList" in input_data:
-            self.alt_loc_list = [chr(int(a)) for a in self.run_length_decoder_numpy(np.frombuffer(input_data[b'altLocList'][12:],">i4"))]
-            #self.alt_loc_list = self.run_length_decoder_numpy(np.frombuffer(input_data[b'altLocList'][12:],">i4")).astype(np.int16)
 
-        else:
-            self.alt_loc_list = []
 
         if b"insCodeList" in input_data:
             self.ins_code_list = self.run_length_decoder_numpy(np.frombuffer(input_data[b"insCodeList"][12:],">i4"))
@@ -258,3 +199,34 @@ class mmtf_structure(object):
             self.sequence_index_list = np.cumsum(self.run_length_decoder_numpy(np.frombuffer(input_data[b'sequenceIndexList'][12:],'>i4')).astype(np.int16))
         else:
             self.sequence_index_list = []
+
+
+
+
+        # TODO dictionary in bytes
+        if b"entityList" in input_data:
+            self.entity_list = self.decode_entity_list(input_data[b"entityList"])
+        else:
+            self.entity_list = []
+        self.group_list = self.decode_group_list(input_data[b"groupList"])
+
+
+
+
+        self.chain_id_list = [chr(a) for a in input_data[b"chainIdList"][12:][::4]]
+
+        if b"chainNameList" in input_data:
+            self.chain_name_list = [chr(a) for a in input_data[b"chainNameList"][12:][::4]]
+        else:
+            self.chain_name_list = []
+        if b"experimentalMethods" in input_data:
+            self.experimental_methods = [x.decode() for x in input_data[b"experimentalMethods"]]
+        else:
+            self.experimental_methods = None
+        '''
+        if b"altLocList" in input_data:
+            self.alt_loc_list = [chr(a) for a in self.run_length_decoder_numpy(np.frombuffer(input_data[b'altLocList'][12:],">i4")).astype(np.int16)]
+            #self.alt_loc_list = self.run_length_decoder_numpy(np.frombuffer(input_data[b'altLocList'][12:],">i4")).astype(np.int16)
+        else:
+            self.alt_loc_list = []
+        '''
