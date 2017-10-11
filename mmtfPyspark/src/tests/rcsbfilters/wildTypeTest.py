c4ee425264dca1eb1f9a1b95014de6838f81fe25
marshuang80 <marshuang80@gmail.com>
2017-10-11 15:07:38 -0700

    Finished and tested wildTypeQuery

---
+++ mmtfPyspark/src/tests/rcsbfilters/wildTypeTest.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import unittest
+from pyspark import SparkConf, SparkContext
+from src.main.io.MmtfReader import downloadMmtfFiles
+from src.main.rcsbfilters import wildTypeQuery
+
+
+class wildTypeTest(unittest.TestCase):
+
+    def setUp(self):
+        conf = SparkConf().setMaster("local[*]").setAppName('wildTypeTest')
+        self.sc = SparkContext(conf=conf)
+
+        pdbIds = ["1PEN", "1OCZ", "2ONX"]
+        self.pdb = downloadMmtfFiles(pdbIds,self.sc)
+
+
+    def test1(self):
+        pdb_1 = self.pdb.filter(wildTypeQuery(True, wildTypeQuery.SEQUENCE_COVERAGE_100))
+        results_1 = pdb_1.keys().collect()
+
+        self.assertTrue('1PEN' in results_1)
+        self.assertTrue('1OCZ' in results_1)
+        self.assertFalse('2ONX' in results_1)
+
+
+    def tearDown(self):
+        self.sc.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
