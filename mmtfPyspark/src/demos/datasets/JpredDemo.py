e1dcad9a20dfaa1b71ba43797fc45ec2d7b21607
marshuang80 <marshuang80@gmail.com>
2017-10-16 13:36:19 -0700

    JpredDataset implemented and tested

---
+++ mmtfPyspark/src/demos/datasets/JpredDemo.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+'''
+JpredDemo.py:
+
+
+Authorship information:
+    __author__ = "Mars Huang"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmail.com:
+    __status__ = "Done"
+'''
+
+from pyspark import SparkConf, SparkContext, SQLContext
+from src.main.datasets import JpredDataset
+import time
+
+
+def main():
+    start = time.time()
+
+    conf = SparkConf().setMaster("local[*]") \
+                      .setAppName("JpredDemo")
+    sc = SparkContext(conf = conf)
+
+    # Read Jpred Dataset
+    res = JpredDataset.getDataset(sc)
+    res.show(10)
+
+    # Write to Json file
+    res = res.coalesce(1)
+    res.write.format("json").save("/home/marshuang80/backup/JpredData3")
+
+
+    end = time.time()
+
+    print("Time: %f  sec." %(end-start))
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
+
+
---
+++ mmtfPyspark/src/main/datasets/JpredDataset.py
@@ -0,0 +1,73 @@
+'''
+JpredDataset.py
+
+This class downloads the dataset used to train the
+<a href="http://www.compbio.dundee.ac.uk/jpred/about_RETR_JNetv231_details.shtml">JPred 4/JNet (v.2.3.1)</a>
+secondary structure predictor. It can be used as a reference
+dataset for machine learning applications.
+This dataset includes the ScopID, sequence, DSSP secondary
+structure assignment, and a flag that indicates if data point
+was part of the training set.
+
+Authorship information:
+    __author__ = "Mars Huang"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmail.com:
+    __status__ = "Done"
+'''
+
+import urllib.request
+import tarfile
+from pyspark.sql import Row
+from src.main.ml import pythonRDDToDataset
+
+def getDataset(sc):
+    '''
+    Gets JPred 4/JNet (v.2.3.1) secondary structure dataset.
+
+    Attribute:
+        sc (SparkContext)
+
+    Return:
+        secondaryStructure dataset
+    '''
+
+    URL = "http://www.compbio.dundee.ac.uk/jpred/downloads/retr231.tar.gz"
+    instream = urllib.request.urlopen(URL)
+    secondaryStructures, sequences, trained = {}, {}, {}
+    scopIds = set()
+    res = []
+
+    with tarfile.open(fileobj=instream, mode = "r:gz") as tf:
+
+        for entry in tf:
+            if entry.isdir():
+                continue
+            br = tf.extractfile(entry)
+
+            if ".dssp" in entry.name:
+                scopID = str(br.readline())[3:-3] # Remove newline and byte
+                secondaryStructure = str(br.readline())[2:-3] # Remove newline and byte
+                secondaryStructure = secondaryStructure.replace('-','C')
+                secondaryStructures[scopID] = secondaryStructure
+
+            if ".fasta" in entry.name:
+                scopID = str(br.readline())[3:-3] # Remove newline and byte
+                sequence = str(br.readline())[2:-3] # Remove newline and byte
+                scopIds.add(scopID)
+                sequences[scopID] = sequence
+
+                if "training/" in entry.name:
+                    trained[scopID] = "true"
+                elif "blind/" in entry.name:
+                    trained[scopID] = "false"
+
+    for scopId in scopIds:
+        row = Row(scopId, sequences[scopId], secondaryStructures[scopId], trained[scopId])
+        res.append(row)
+
+    data = sc.parallelize(res)
+    colNames = ["scopID","sequence", "secondaryStructure", "trained"]
+
+    return pythonRDDToDataset.getDataset(data, colNames)
+
