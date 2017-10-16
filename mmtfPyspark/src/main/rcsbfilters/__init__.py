a220f29256bfaa5ceb4b3b69fd1e92c89e63e0f1
marshuang80 <marshuang80@gmail.com>
2017-10-16 13:58:02 -0700

    Updated rcsbfilters init file

--- mmtfPyspark/src/main/rcsbfilters/__init__.py
+++ mmtfPyspark/src/main/rcsbfilters/__init__.py
@@ -1,2 +1,6 @@
 from .pisces import pisces
 from .advancedQuery import advancedQuery
+from .chemicalStructureQuery import chemicalStructureQuery
+from .sequenceSimilarity import sequenceSimilarity
+from .wildTypeQuery import wildTypeQuery
+from .customReportQuery import customReportQuery
