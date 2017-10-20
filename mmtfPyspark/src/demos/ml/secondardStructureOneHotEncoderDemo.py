8073bc08bbae52094f4bf7f0757ef7d550090631
marshuang80 <marshuang80@gmail.com>
2017-10-19 18:26:14 -0700

    OneHotEncoding working, but has more data than java

--- mmtfPyspark/src/demos/ml/secondardStructureOneHotEncoderDemo.py
+++ mmtfPyspark/src/demos/ml/secondardStructureOneHotEncoderDemo.py
@@ -10,11 +10,12 @@ Authorship information:
 '''
 
 from pyspark import SparkConf, SparkContext, SQLContext
-from src.ml.proteinSequenceEncoder import oneHotEncode
-from src.mapper.structureToPolymerChains
-from src.filters.containsLProteinChain
-from src.datasets.secondaryStructureSegmentExtractor
-from rcsbfilters import pisces
+from src.main.ml import proteinSequenceEncoder
+from src.main.mappers import structureToPolymerChains
+from src.main.filters import containsLProteinChain
+from src.main.datasets import secondaryStructureSegmentExtractor
+from src.main.rcsbfilters import pisces
+from src.main.io import MmtfReader
 import time
 
 
@@ -29,26 +30,32 @@ def main():
     # Read MMTF Hadoop sequence file and create a non-redundant set
     # (<=20% seq. identity) of L-protein chains
 
+    path = "/home/marshuang80/PDB/reduced"
+
     sequenceIdentity = 20
     resolution = 2.0
     fraction = 0.1
     seed = 123
 
+    #.downloadMmtfFiles(["2ONX",'1JLP','5X6H','5L2G','2MK1' ],sc) \
+
+
+
     pdb = MmtfReader \
             .readSequenceFile(path, sc) \
             .flatMap(structureToPolymerChains()) \
-            .filter(pisces(sequenceIdentity, resolution) \
-            .filter(containsLProteinChain) \
+            .filter(pisces(sequenceIdentity, resolution)) \
+            .filter(containsLProteinChain()) \
             .sample(False, fraction, seed)
 
     segmentLength = 11
     data = secondaryStructureSegmentExtractor.getDataset(pdb, segmentLength).cache()
     print(f"original data   : {data.count()}")
 
-    data = data.dropDuplicates("labelQ3", "sequence").cache()
+    data = data.dropDuplicates(["labelQ3", "sequence"]).cache()
     print(f"- duplicate Q3/seq  : {data.count()}")
 
-    data = data.dropDuplicates("sequences")
+    data = data.dropDuplicates(["sequence"])
     print(f"- duplicate seq  : {data.count()}")
 
     encoder = proteinSequenceEncoder(data)
