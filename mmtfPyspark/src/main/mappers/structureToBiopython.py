9939c601ba47efc038ac6e6da6f955d10263689f
marshuang80 <marshuang80@gmail.com>
2017-11-19 16:03:19 -0800

    Error when using MMTFEncoder instead of mmtfStructure

--- mmtfPyspark/src/main/mappers/structureToBiopython.py
+++ mmtfPyspark/src/main/mappers/structureToBiopython.py
@@ -11,13 +11,15 @@ Authorship information:
     __status__ = "debug"
 '''
 from Bio.PDB.mmtf import DefaultParser
+from mmtf.utils import decoder_utils
+
+# TODO MMTFEncoder error
 
 class structureToBiopython(object):
 
     def __call__(self, t):
 
         parser = DefaultParser.StructureDecoder()
-
         t.pass_data_on(parser)
 
         return parser.structure_bulder.get_structure()
