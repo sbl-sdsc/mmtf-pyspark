7aac7995240d179010bfe77b11e4c2dd03f5781b
marshuang80 <marshuang80@gmail.com>
2017-11-27 11:27:08 -0800

    Renamed rcsbfilters to webfilters

--- mmtfPyspark/src/main/rcsbfilters/__init__.py
+++
@@ -1,7 +0,0 @@
-from .pisces import pisces
-from .advancedQuery import advancedQuery
-from .chemicalStructureQuery import chemicalStructureQuery
-from .sequenceSimilarity import sequenceSimilarity
-from .wildTypeQuery import wildTypeQuery
-from .customReportQuery import customReportQuery
-from .blastCluster import blastCluster
