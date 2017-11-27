e7501cca04a221c4732d3b10b3a76db16f412c1e
marshuang80 <marshuang80@gmail.com>
2017-11-27 13:06:40 -0800

    rcsbfilters to webfilters

---
+++ tests/webfilters/blastClustersTest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import unittest
+from pyspark import SparkConf, SparkContext
+from mmtfPyspark.io.MmtfReader import downloadMmtfFiles
+from mmtfPyspark.rcsbfilters import blastCluster
+from mmtfPyspark.mappers import structureToPolymerChains
+
+
+class blastClustersTest(unittest.TestCase):
+
+    def setUp(self):
+        conf = SparkConf().setMaster("local[*]").setAppName('blastClustersTest')
+        self.sc = SparkContext(conf=conf)
+
+        pdbIds = ["1O06","2ONX"]
+        self.pdb = downloadMmtfFiles(pdbIds,self.sc)
+
+
+    def test1(self):
+
+        pdb_1 = self.pdb.filter(blastCluster(40))
+        results_1 = pdb_1.keys().collect()
+
+        self.assertTrue('1O06' in results_1)
+        self.assertFalse('1O06.A' in results_1)
+        self.assertFalse('2ONX' in results_1)
+
+
+    def test2(self):
+
+        pdb_2 = self.pdb.filter(blastCluster(40))
+        pdb_2 = pdb_2.flatMap(structureToPolymerChains())
+        results_2 = pdb_2.keys().collect()
+
+        self.assertFalse('1O06' in results_2)
+        self.assertTrue('1O06.A' in results_2)
+        self.assertFalse('2ONX' in results_2)
+
+
+    def tearDown(self):
+        self.sc.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/webfilters/chemicalStructureQueryTest.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import unittest
+from pyspark import SparkConf, SparkContext
+from mmtfPyspark.io.MmtfReader import downloadMmtfFiles
+from mmtfPyspark.rcsbfilters import chemicalStructureQuery
+from mmtfPyspark.mappers import structureToPolymerChains
+
+
+class chemicalStructureQueryTest(unittest.TestCase):
+
+    def setUp(self):
+        conf = SparkConf().setMaster("local[*]").setAppName('chemicalStructureQueryTest')
+        self.sc = SparkContext(conf=conf)
+
+        pdbIds = ["1HYA", "2ONX", "1F27", "4QMC", "2RTL"]
+        self.pdb = downloadMmtfFiles(pdbIds,self.sc)
+
+
+    def test1(self):
+        smiles = "CC(=O)NC1C(O)OC(CO)C(O)C1O"
+
+        pdb_1 = self.pdb.filter(chemicalStructureQuery(smiles))
+        results_1 = pdb_1.keys().collect()
+
+        self.assertTrue('1HYA' in results_1)
+        self.assertFalse('2ONX' in results_1)
+
+
+    def test2(self):
+        smiles = "OC(=O)CCCC[C@@H]1SC[C@@H]2NC(=O)N[C@H]12"
+        queryType = chemicalStructureQuery.EXACT
+        percentSimilarity = 0
+
+        pdb_2 = self.pdb.filter(chemicalStructureQuery(smiles, queryType, percentSimilarity))
+        results_2 = pdb_2.keys().collect()
+
+        self.assertFalse('1HYA' in results_2)
+        self.assertFalse('2ONX' in results_2)
+        self.assertTrue('1F27' in results_2)
+        self.assertFalse('2RTL' in results_2)
+        self.assertFalse('4QMC' in results_2)
+
+
+    def test3(self):
+        smiles = "OC(=O)CCCC[C@@H]1SC[C@@H]2NC(=O)N[C@H]12"
+        queryType = chemicalStructureQuery.SUBSTRUCTURE
+        percentSimilarity = 0
+
+        pdb_3 = self.pdb.filter(chemicalStructureQuery(smiles, queryType, percentSimilarity))
+        results_3 = pdb_3.keys().collect()
+
+        self.assertFalse('1HYA' in results_3)
+        self.assertFalse('2ONX' in results_3)
+        self.assertTrue('1F27' in results_3)
+        self.assertFalse('2RTL' in results_3)
+        self.assertTrue('4QMC' in results_3)
+
+
+    def test4(self):
+        smiles = "OC(=O)CCCC[C@@H]1SC[C@@H]2NC(=O)N[C@H]12"
+        queryType = chemicalStructureQuery.SIMILAR
+        percentSimilarity = 70
+
+        pdb_4 = self.pdb.filter(chemicalStructureQuery(smiles, queryType, percentSimilarity))
+        results_4 = pdb_4.keys().collect()
+
+        self.assertFalse('1HYA' in results_4)
+        self.assertFalse('2ONX' in results_4)
+        self.assertTrue('1F27' in results_4)
+        self.assertTrue('2RTL' in results_4)
+        self.assertTrue('4QMC' in results_4)
+
+
+    def test5(self):
+        smiles = "OC(=O)CCCC[C@H]1[C@H]2NC(=O)N[C@H]2C[S@@]1=O"
+        queryType = chemicalStructureQuery.SUPERSTRUCTURE
+        percentSimilarity = 0
+
+        pdb_5 = self.pdb.filter(chemicalStructureQuery(smiles, queryType, percentSimilarity))
+        results_5 = pdb_5.keys().collect()
+
+        self.assertFalse('1HYA' in results_5)
+        self.assertFalse('2ONX' in results_5)
+        self.assertTrue('1F27' in results_5)
+        self.assertFalse('2RTL' in results_5)
+        self.assertTrue('4QMC' in results_5)
+
+    def tearDown(self):
+        self.sc.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
