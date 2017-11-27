5b755857b439fec95f0306184e8dde6cfe4467f4
marshuang80 <marshuang80@gmail.com>
2017-11-27 11:30:17 -0800

    pdbjMine filter moved to webfilters

---
+++ mmtfPyspark/src/demos/webfilters/keywordSearch.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+'''
+keywordSearch.py
+
+PDBj Mine 2 RDB keyword search query and MMTF filtering using pdbid
+This filter searches the 'keyword' column in the brief_summary table for
+a keyword and returns a couple of columns for the matching entries
+
+Authorship information:
+    __author__ = "Mars Huang"
+    __maintainer__ = "Mars Huang"
+    __email__ = "marshuang80@gmail.com:
+    __status__ = "Done"
+'''
+
+from pyspark import SparkConf, SparkContext
+from src.main.webfilters import PdbjMine
+from src.main.datasets import PdbjMineService
+from src.main.io import MmtfReader
+
+def main():
+    path = "/home/marshuang80/PDB/full"
+
+    conf = SparkConf().setMaster("local[*]") \
+                      .setAppName("keywordSearch")
+    sc = SparkContext(conf = conf)
+
+    pdb = MmtfReader.readSequenceFile(path, sc)
+    sql = "select pdbid, resolution, biol_species, db_uniprot, db_pfam, hit_score from keyword_search('porin') order by hit_score desc"
+
+    search = PdbjMine(sql)
+    count = pdb.filter(search).keys().count()
+    print(f"Number of entries using sql to filter: {count}")
+
+    dataset = PdbjMineService.getDataset(sql)
+    dataset.show(10)
+    search = PdbjMine(dataset = dataset)
+    count = pdb.filter(search).keys().count()
+    print(f"Number of entries using dataset to filter: {count}")
+
+    sc.stop()
+
+if __name__ == "__main__":
+    main()
