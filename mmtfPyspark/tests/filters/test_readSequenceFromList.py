b91305f17d956edc77998291c50d4d81673a3b90
marshuang80 <marshuang80@gmail.com>
2017-11-27 14:28:59 -0800

    Edited readSequenceFreomList test

--- mmtfPyspark/tests/filters/test_readSequenceFromList.py
+++ mmtfPyspark/tests/filters/test_readSequenceFromList.py
@@ -14,23 +14,28 @@ import unittest
 from pyspark import SparkConf, SparkContext
 from mmtfPyspark.io.MmtfReader import readSequenceFile
 
-path = '../full'
-stringIds = "1AQ1,1B38,1B39,1BUH,1C25,1CKP,1DI8,1DM2,1E1V,1E1X,1E9H,1F5Q,1FIN,1FPZ,1FQ1,1FQV,1FS1"
-pdbIds = stringIds.split(',')
 
 class testReadSequenceFile(unittest.TestCase):
+
+    def setUp(self):
+        path = '../full'
+        stringIds = "1AQ1,1B38,1B39,1BUH,1C25,1CKP,1DI8,1DM2,1E1V,1E1X,1E9H,1F5Q,1FIN,1FPZ,1FQ1,1FQV,1FS1"
+        pdbIds = stringIds.split(',')
+        conf = SparkConf().setMaster("local[*]").setAppName('Demo0b')
+        sc = SparkContext(conf=conf)
+        pdb = readSequenceFile(path, sc, pdbId = pdbIds)
+
 
     def test_size(self):
         self.assertEqual(len(pdbIds),pdb.count())
 
+
     def test_result(self):
         self.assertEqual(set(pdbIds),set(pdb.keys().collect()))
+
 
     def tearDown(self):
         sc.stop()
 
 if __name__ == '__main__':
-    conf = SparkConf().setMaster("local[*]").setAppName('Demo0b')
-    sc = SparkContext(conf=conf)
-    pdb = readSequenceFile(path, sc, pdbId = pdbIds)
     unittest.main()
