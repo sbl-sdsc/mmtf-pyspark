34bb122d79bc2e61a4592bae47da0631f6c3dcaa
marshuang80 <marshuang80@gmail.com>
2017-11-28 14:50:30 -0800

    PdbjMineSearch tested

---
+++ mmtfPyspark/tests/webfilters/test_mineSearch.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+import unittest
+from pyspark import SparkConf, SparkContext
+from mmtfPyspark.io.MmtfReader import downloadMmtfFiles
+from mmtfPyspark.webfilters import PdbjMine
+from mmtfPyspark.datasets import PdbjMineService
+from mmtfPyspark.mappers import structureToPolymerChains
+
+
+class mineSearchTest(unittest.TestCase):
+
+    def setUp(self):
+        conf = SparkConf().setMaster("local[*]").setAppName('mineSearchTest')
+        self.sc = SparkContext(conf=conf)
+
+        pdbIds = ["5JDE","5CU4","5L6W","5UFU","5IHB"]
+        self.pdb = downloadMmtfFiles(pdbIds,self.sc)
+
+
+    def test1(self):
+        sql = "select count(*) from brief_summary"
+
+        search = PdbjMineService.getDataset(sql)
+
+        self.assertTrue(search.head()[0] > 100000)
+
+
+    def test2(self):
+        sql ="select distinct entity.pdbid from entity join entity_src_gen on entity_src_gen.pdbid=entity.pdbid where pdbx_ec='2.7.11.1' and pdbx_gene_src_scientific_name='Homo sapiens'"
+
+        pdb = self.pdb.filter(PdbjMine(sql))
+        matches = pdb.keys().collect()
+
+        self.assertTrue("5JDE" in matches)
+        self.assertTrue("5CU4" in matches)
+        self.assertTrue("5L6W" in matches)
+        self.assertFalse("5UFU" in matches)
+        self.assertFalse("5IHB" in matches)
+
+
+    def test3(self):
+        sql = "select distinct concat(entity_poly.pdbid, '.', unnest(string_to_array(entity_poly.pdbx_strand_id, ','))) as \"structureChainId\" from entity_poly join entity_src_gen on entity_src_gen.pdbid=entity_poly.pdbid and entity_poly.entity_id=entity_poly.entity_id join entity on entity.pdbid=entity_poly.pdbid and entity.id=entity_poly.entity_id where pdbx_ec='2.7.11.1' and pdbx_gene_src_scientific_name='Homo sapiens'"
+
+        pdb = self.pdb.flatMap(structureToPolymerChains()) \
+                      .filter(PdbjMine(sql, pdbidField = "structureChainId", chainLevel = True))
+
+        matches = pdb.keys().collect()
+
+        self.assertTrue("5JDE.A" in matches)
+        self.assertTrue("5JDE.B" in matches)
+        self.assertTrue("5CU4.A" in matches)
+        self.assertTrue("5L6W.L" in matches)
+        self.assertFalse("5L6W.C" in matches)
+        self.assertFalse("5UFU.A" in matches)
+        self.assertFalse("5UFU.B" in matches)
+        self.assertFalse("5UFU.C" in matches)
+        self.assertFalse("5IHB.A" in matches)
+        self.assertFalse("5IHB.B" in matches)
+        self.assertFalse("5IHB.C" in matches)
+        self.assertFalse("5IHB.D" in matches)
+
+
+    def tearDown(self):
+        self.sc.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
