eafcdd3f55157a36e5adf1919117448c1134d83f
marshuang80 <marshuang80@gmail.com>
2017-12-22 11:58:44 -0800

    Updated testcases for Travis

--- mmtfPyspark/tests/io/test_readSequenceFromList.py
+++ mmtfPyspark/tests/io/test_readSequenceFromList.py
@@ -4,9 +4,9 @@ readSequenceFromListTest.py: Example reading a list of PDB IDs from a local MMTF
 file into a tubleRDD.
 
 Authorship information:
-__author__ = "Peter Rose"
+__author__ = "Mars Huang"
 __maintainer__ = "Mars Huang"
-__email__ = "marshuang80@gmai.com:
+__email__ = "marshuang80@gmail.com:
 __status__ = "Warning"
 '''
 
@@ -18,24 +18,24 @@ from mmtfPyspark.io.MmtfReader import readSequenceFile
 class testReadSequenceFile(unittest.TestCase):
 
     def setUp(self):
-        path = '../full'
+        path = '/home/marshuang80/PDB/full'
         stringIds = "1AQ1,1B38,1B39,1BUH,1C25,1CKP,1DI8,1DM2,1E1V,1E1X,1E9H,1F5Q,1FIN,1FPZ,1FQ1,1FQV,1FS1"
-        pdbIds = stringIds.split(',')
-        conf = SparkConf().setMaster("local[*]").setAppName('Demo0b')
-        sc = SparkContext(conf=conf)
-        pdb = readSequenceFile(path, sc, pdbId = pdbIds)
+        self.pdbIds = stringIds.split(',')
+        conf = SparkConf().setMaster("local[*]").setAppName('readSequenceFile')
+        self.sc = SparkContext(conf=conf)
+        self.pdb = readSequenceFile(path, self.sc, pdbId = self.pdbIds)
 
 
     def test_size(self):
-        self.assertEqual(len(pdbIds),pdb.count())
+        self.assertEqual(len(self.pdbIds),self.pdb.count())
 
 
     def test_result(self):
-        self.assertEqual(set(pdbIds),set(pdb.keys().collect()))
+        self.assertEqual(set(self.pdbIds),set(self.pdb.keys().collect()))
 
 
     def tearDown(self):
-        sc.stop()
+        self.sc.stop()
 
 if __name__ == '__main__':
     unittest.main()
