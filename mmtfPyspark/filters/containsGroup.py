3744e14ba9f93de775c97a1527b8948a0b931cf3
marshuang80 <marshuang80@gmail.com>
2018-02-02 13:59:54 -0800

    [Fix] fixed #7 issue

--- mmtfPyspark/filters/containsGroup.py
+++ mmtfPyspark/filters/containsGroup.py
@@ -2,9 +2,10 @@
 '''
 containsGroup.py:
 
-This filter returns entries that contain specified groups (residues).
-Groups are specified by their one, two, or three-letter codes, e.g. "F", "MG", "ATP", as defined
-in the <a href="https://www.wwpdb.org/data/ccd">wwPDB Chemical Component Dictionary</a>.
+This filter returns entries that contain at least one of the specified groups
+(residues).Groups are specified by their one, two, or three-letter codes,
+e.g. "F", "MG", "ATP", as defined in the
+<a href="https://www.wwpdb.org/data/ccd">wwPDB Chemical Component Dictionary</a>.
 
 Authorship information:
     __author__ = "Mars (Shih-Cheng) Huang"
@@ -27,7 +28,7 @@ class containsGroup(object):
         groups = [t[1].group_list[idx]['groupName'] for idx in t[1].group_type_list]
 
         for group in self.groupQuery:
-            if group not in groups:
-                return False
+            if group in groups:
+                return True
 
-        return True
+        return False
