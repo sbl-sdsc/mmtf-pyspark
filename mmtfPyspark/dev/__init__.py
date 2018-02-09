7fc1d912a7cd00c5d47664837a240ecafd2af02d
marshuang80 <marshuang80@gmail.com>
2018-02-08 16:30:01 -0800

    [NEW] Implemented interactionCenter, still need to debug

--- mmtfPyspark/dev/__init__.py
+++ mmtfPyspark/dev/__init__.py
@@ -1,2 +1,4 @@
 from .columnarStructure import ColumnarStructure
 from .columnarStructureX import ColumnarStructureX
+from .interactionFilter import InteractionFilter
+from .interactionCenter import InteractionCenter
