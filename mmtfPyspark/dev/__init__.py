e38b0cc75b7d3a5f3417bcdedb12298c214f80af
marshuang80 <marshuang80@gmail.com>
2018-02-14 12:03:17 -0800

    [UPDATE] init file to include atomInteraction

--- mmtfPyspark/dev/__init__.py
+++ mmtfPyspark/dev/__init__.py
@@ -2,3 +2,5 @@ from .columnarStructure import ColumnarStructure
 from .columnarStructureX import ColumnarStructureX
 from .interactionFilter import InteractionFilter
 from .interactionCenter import InteractionCenter
+from .coordinateGeometry import CoordinateGeometry
+from .atomInteraction import AtomInteraction
