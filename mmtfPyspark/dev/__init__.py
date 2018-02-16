4cb57ceddbea22dea7b4f7fc3e428de2d97341e2
marshuang80 <marshuang80@gmail.com>
2018-02-15 19:28:08 -0800

    [FIX] GroupInteractionExtractor class name

--- mmtfPyspark/dev/__init__.py
+++ mmtfPyspark/dev/__init__.py
@@ -5,3 +5,4 @@ from .interactionCenter import InteractionCenter
 from .coordinationGeometry import CoordinateGeometry
 from .atomInteraction import AtomInteraction
 from .structureToAtomInteractions import StructureToAtomInteractions
+from .groupInteractionExtractor import GroupInteractionExtractor
