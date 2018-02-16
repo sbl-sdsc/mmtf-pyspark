f249175ff94f3eda51984282a1f5c51d4178edd0
marshuang80 <marshuang80@gmail.com>
2018-02-15 19:09:22 -0800

    [EDIT] renamed structureToAtomInteractions

--- mmtfPyspark/dev/__init__.py
+++ mmtfPyspark/dev/__init__.py
@@ -4,3 +4,4 @@ from .interactionFilter import InteractionFilter
 from .interactionCenter import InteractionCenter
 from .coordinationGeometry import CoordinateGeometry
 from .atomInteraction import AtomInteraction
+from .structureToAtomInteractions import StructureToAtomInteractions
