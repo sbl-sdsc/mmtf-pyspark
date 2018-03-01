b640b25e6dbc434d659d86119db805baf92e6304
marshuang80 <marshuang80@gmail.com>
2018-02-28 17:03:05 -0800

    [FORMAT] mmtfwriter functions to match PEP8 standards

--- mmtfPyspark/io/mmtfWriter.py
+++ mmtfPyspark/io/mmtfWriter.py
@@ -1,6 +1,5 @@
 #!/user/bin/env python
-'''
-mmtfWriter.py
+'''mmtfWriter.py
 
 Encodes and write MMTF encoded structure data to a Hadoop Sequence File
 
@@ -17,35 +16,37 @@ import gzip
 import msgpack
 import os
 
-def writeSequenceFile(path, sc, structure, compressed = True):
-    '''
-    Encodes and writes MMTF encoded structure data to a Hadoop Sequnce File
 
-    Attributes:
+def write_sequence_file(path, sc, structure, compressed=True):
+    '''Encodes and writes MMTF encoded structure data to a Hadoop Sequnce File
+
+    Attributes
+    ----------
         path (str): Path to Hadoop file directory)
         sc (Spark context)
         structure (tuple): structure data to be written
         compress (bool): if true, apply gzip compression
     '''
     if structure.first()[1] == MmtfStructure:
-        structure.map(lambda s: (s[0],s[1].set_alt_loc_list()) \
-                                 if not s[1].alt_loc_set \
-                                 else s) \
+        structure.map(lambda s: (s[0], s[1].set_alt_loc_list())
+                      if not s[1].alt_loc_set
+                      else s) \
 
-    structure.map(lambda t: (t[0], toByteArray(t[1], compressed))).saveAsHadoopFile(path,
-                       "org.apache.hadoop.mapred.SequenceFileOutputFormat",
-                       "org.apache.hadoop.io.Text",
-                       "org.apache.hadoop.io.BytesWritable")
+    structure.map(lambda t: (t[0], _to_byte_array(t[1], compressed)))\
+             .saveAsHadoopFile(path,
+                               "org.apache.hadoop.mapred.SequenceFileOutputFormat",
+                               "org.apache.hadoop.io.Text",
+                               "org.apache.hadoop.io.BytesWritable")
 
 
-def writeMmtfFiles(path, sc, structure):
-    '''
-    Encodes and writes MMTF encoded and gzipped structure data to individual .mmtf.gz files.
+def write_mmtf_files(path, sc, structure):
+    '''Encodes and writes MMTF encoded and gzipped structure data to individual .mmtf.gz files.
 
-    Attributes:
-    path (str): Path to Hadoop file directory)
-    sc (Spark context)
-    structure (tuple): structure data to be written
+    Attributes
+    ----------
+        path (str): Path to Hadoop file directory)
+        sc (Spark context)
+        structure (tuple): structure data to be written
     '''
 
     if path[-1] != "/":
@@ -54,18 +55,18 @@ def writeMmtfFiles(path, sc, structure):
     if not os.path.exists(path):
         os.makedirs(path)
 
-    structure = structure.map(lambda s: (s[0],s[1].set_alt_loc_list()) \
-                                  if not s[1].alt_loc_set \
-                                  else s) \
-                .map(lambda t: (t[0], toByteArray(t[1], False))) \
-                .foreach(lambda t: gzip.open(path + t[0] + '.mmtf.gz', mode = 'wb').write(t[1]))
+    structure = structure.map(lambda s: (s[0], s[1].set_alt_loc_list())
+                              if not s[1].alt_loc_set
+                              else s) \
+        .map(lambda t: (t[0], _to_byte_array(t[1], False))) \
+        .foreach(lambda t: gzip.open(path + t[0] + '.mmtf.gz', mode='wb').write(t[1]))
 
 
-def toByteArray(structure, compressed):
-    '''
-    Returns an MMTF-encoded byte array with optional gzip compression
+def _to_byte_array(structure, compressed):
+    '''Returns an MMTF-encoded byte array with optional gzip compression
 
-    Returns:
+    Returns
+    -------
         MMTF encoded and optionally gzipped structure data
     '''
 
