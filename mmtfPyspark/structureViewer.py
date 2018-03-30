7f8bcc5f610edcd6fc964e2ea9c4dae7c95f0a87
marshuang80 <marshuang80@gmail.com>
2018-03-29 18:25:45 -0700

    [UPDATE] group_interaction_viewer to take parameters

--- mmtfPyspark/structureViewer.py
+++ mmtfPyspark/structureViewer.py
@@ -13,9 +13,11 @@ Authorship information:
 '''
 
 from ipywidgets import interact, IntSlider
+import numpy as np
 import py3Dmol
 
-def simple_structure_viewer(pdbIds, style = 'cartoon', color = 'spectrum'):
+
+def simple_structure_viewer(pdbIds, style='cartoon', color='spectrum'):
     '''A wrapper function that simply displays a list of protein structures using
     ipywidgets and py3Dmol
 
@@ -29,7 +31,7 @@ def simple_structure_viewer(pdbIds, style = 'cartoon', color = 'spectrum'):
     if type(pdbIds) == str:
         pdbIds = [pdbIds]
 
-    def view3d(i = 0):
+    def view3d(i=0):
         '''Simple structure viewer that uses py3Dmol to view PDB structure by
         indexing the list of PDBids
 
@@ -40,15 +42,15 @@ def simple_structure_viewer(pdbIds, style = 'cartoon', color = 'spectrum'):
 
         print(f"PdbID: {pdbIds[i]}, Style: {style}")
 
-        viewer = py3Dmol.view(query='pdb:'+pdbIds[i])
+        viewer = py3Dmol.view(query='pdb:' + pdbIds[i])
         viewer.setStyle({style: {'color': color}})
 
         return viewer.show()
 
-    return interact(view3d, i=(0,len(pdbIds)-1))
+    return interact(view3d, i=(0, len(pdbIds) - 1))
 
 
-def interaction_structure_viewer(pdbIds, interacting_atom = 'None', style = 'cartoon', color = 'spectrum'):
+def interaction_structure_viewer(pdbIds, interacting_atom='None', style='cartoon', color='spectrum'):
     '''A wrapper function that simply displays a list of protein structures using
     ipywidgets and py3Dmol and highlight specified interacting groups
 
@@ -63,7 +65,7 @@ def interaction_structure_viewer(pdbIds, interacting_atom = 'None', style = 'car
     if type(pdbIds) == str:
         pdbIds = [pdbIds]
 
-    def view3d(i = 0):
+    def view3d(i=0):
         '''Simple structure viewer that uses py3Dmol to view PDB structure by
         indexing the list of PDBids
 
@@ -72,22 +74,23 @@ def interaction_structure_viewer(pdbIds, interacting_atom = 'None', style = 'car
             i (int): index of the protein if a list of PDBids
         '''
 
-        print(f"PdbID: {pdbIds[i]}, Interactions: {interacting_atom}, Style: {style}")
+        print(
+            f"PdbID: {pdbIds[i]}, Interactions: {interacting_atom}, Style: {style}")
 
-        viewer = py3Dmol.view(query='pdb:'+pdbIds[i])
+        viewer = py3Dmol.view(query='pdb:' + pdbIds[i])
         viewer.setStyle({style: {'color': color}})
 
         if interacting_atom != "None":
 
-            viewer.setStyle({'atom': interacting_atom},{'sphere': {'color':'gray'}})
+            viewer.setStyle({'atom': interacting_atom}, {
+                            'sphere': {'color': 'gray'}})
 
         return viewer.animate()
 
-    return interact(view3d, i = (0,len(pdbIds)-1))
+    return interact(view3d, i=(0, len(pdbIds) - 1))
 
 
-
-def group_neighbor_viewer(pdbIds = None, groups = None, chains = None, distance = 3.0):
+def group_neighbor_viewer(pdbIds=None, groups=None, chains=None, distance=3.0):
     '''A wrapper function that zooms in to a group of a protein structure and highlight
     its neighbors within a certain distance.
 
@@ -106,7 +109,8 @@ def group_neighbor_viewer(pdbIds = None, groups = None, chains = None, distance 
         raise ValueError("PdbIds and groups need to be specified")
 
     if len(pdbIds) != len(groups):
-        raise ValueError("Number of structures should match with number of groups")
+        raise ValueError(
+            "Number of structures should match with number of groups")
 
     if type(pdbIds) == str and groups == str:
         pdbIds, groups = [pdbIds], [groups]
@@ -114,7 +118,7 @@ def group_neighbor_viewer(pdbIds = None, groups = None, chains = None, distance 
     if chains == None:
         chains = ['A'] * len(pdbIds)
 
-    def view3d(i = 0):
+    def view3d(i=0):
         '''Simple structure viewer that zooms into a specified group and highlight
         its neighbors
 
@@ -123,23 +127,25 @@ def group_neighbor_viewer(pdbIds = None, groups = None, chains = None, distance 
             i (int): index of the protein if a list of PDBids
         '''
 
-        print(f"PDB: {pdbIds[i]}, group: {groups[i]}, chain: {chains[i]}, cutoffDistance: {distance}")
+        print(
+            f"PDB: {pdbIds[i]}, group: {groups[i]}, chain: {chains[i]}, cutoffDistance: {distance}")
 
-        center = {'resi':groups[i],'chain':chains[i]}
-        neighbors = {'resi':groups[i],'chain':chains[i],'byres':'true','expand': distance}
+        center = {'resi': groups[i], 'chain': chains[i]}
+        neighbors = {'resi': groups[i], 'chain': chains[i],
+                     'byres': 'true', 'expand': distance}
 
-        viewer = py3Dmol.view(query='pdb:'+pdbIds[i])
+        viewer = py3Dmol.view(query='pdb:' + pdbIds[i])
         viewer.zoomTo(center)
-        viewer.setStyle(neighbors,{'stick':{}});
-        viewer.setStyle(center,{'sphere':{'color':'red'}})
-        viewer.zoom(0.2, 1000);
+        viewer.setStyle(neighbors, {'stick': {}});
+        viewer.setStyle(center, {'sphere': {'color': 'red'}})
+        viewer.zoom(0.2, 1000)
 
         return viewer.show()
 
-    return interact(view3d, i = (0,len(pdbIds)-1))
+    return interact(view3d, i=(0, len(pdbIds) - 1))
 
 
-def group_interaction_viewer(df):
+def group_interaction_viewer(df, sortBy, metal = None):
     '''A wrapper function that zooms in to a group in a protein structure and
     highlight its interacting atoms. The input dataframe should be generated
     from the GroupInteractionExtractor class.
@@ -152,24 +158,34 @@ def group_interaction_viewer(df):
     Attributes
     ----------
         df (dataframe): the dataframe generated from GroupIneteractionExtractor
+        sort_by (str): the q value to sort by ['q4','q5','q6']
     '''
 
-    structures = df['pdbId'].iloc
-    groups = df['groupNum0'].iloc
-    chains = df['chain0'].iloc
-    elements = df['element0'].iloc
+    # Filter by metal
+    if metal is not None:
+        df = df[df["element0"] == metal]
+
+    # Sort dataframe based on sortBy parameter (q4-6 values)
+    df = df.sort_values([sortBy], ascending = False).dropna(subset=[sortBy])
+    if sortBy in ['q4','q5']:
+        q = 'q' + str(int(sortBy[-1]) + 1)
+        df = df[df[q] != np.nan]
+
+    i_widget = IntSlider(
+        min=0, max=df.shape[0] - 1, description='Structure', continuous_update=False)
 
     def get_neighbors_chain(i):
-        return [df[f'chain{j}'].iloc[i] for j in range(1,7) if df[f'element{j}'] is not None]
+        return [df[f'chain{j}'].iloc[i] for j in range(1, 7) if df[f'element{j}'] is not None]
 
     def get_neighbors_group(i):
-        return [df[f'groupNum{j}'].iloc[i] for j in range(1,7) if df[f'element{j}'] is not None]
+        return [df[f'groupNum{j}'].iloc[i] for j in range(1, 7) if df[f'element{j}'] is not None]
 
     def get_neighbors_elements(i):
-        elements = [df[f'element{j}'].iloc[i] for j in range(1,7) if df[f'element{j}'] is not None]
+        elements = [df[f'element{j}'].iloc[i]
+                    for j in range(1, 7) if df[f'element{j}'] is not None]
         return [str(e).upper() for e in elements]
 
-    def view3d(i = 0):
+    def view3d(i=0):
         '''Simple structure viewer that uses py3Dmol to view PDB structure by
         indexing the list of PDBids
         Attributes
@@ -177,18 +193,23 @@ def group_interaction_viewer(df):
         i (int): index of the protein if a list of PDBids
         '''
 
-        print("PDBId: " + structures[i] + " chain: " + chains[i] + " element: " + elements[i])
-        viewer = py3Dmol.view(query='pdb:'+structures[i],width=700,height=700)
+        structures = df['pdbId'].iloc
+        groups = df['groupNum0'].iloc
+        chains = df['chain0'].iloc
+        elements = df['element0'].iloc
+        ori = str(df[sortBy].iloc[i])[:5]
 
-        neighbors = {'resi':get_neighbors_group(i), 'chain':get_neighbors_chain(i)}
-        metal = {'resi':groups[i], 'atom':str(elements[i]).upper(), 'chain':chains[i]}
+        print(f"PDBId: {structures[i]}    chain: {chains[i]}    element:    {elements[i]}")
+        print(f"{sortBy}: {ori}")
 
-        viewer.setStyle(neighbors,{'stick':{'colorscheme':'orangeCarbon'}})
-        viewer.setStyle(metal,{'sphere':{'radius':0.5,'color':'gray'}})
+        viewer = py3Dmol.view(query='pdb:' + structures[i], width=700, height=700)
+        neighbors = {'resi': get_neighbors_group(i), 'chain': get_neighbors_chain(i)}
+        metal = {'resi': groups[i], 'atom': str(elements[i]).upper(), 'chain': chains[i]}
+
+        viewer.setStyle(neighbors, {'stick': {'colorscheme': 'orangeCarbon'}})
+        viewer.setStyle(metal, {'sphere': {'radius': 0.5, 'color': 'gray'}})
 
         viewer.zoomTo(neighbors)
-        return viewer.show();
+        return viewer.show()
 
-    s_widget = IntSlider(min=0,max=df.shape[0]-1,description='Structure',continuous_update=False)
-
-    return interact(view3d, i=s_widget);
+    return interact(view3d, i=i_widget)
