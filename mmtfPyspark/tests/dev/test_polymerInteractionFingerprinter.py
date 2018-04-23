9253144941a8478096dd78f551f840985d2b2aeb
marshuang80 <marshuang80@gmail.com>
2018-04-23 15:06:03 -0700

    [NEW] test for polymerfingerprinter

---
+++ mmtfPyspark/tests/dev/test_polymerInteractionFingerprinter.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import unittest
+from pyspark import SparkConf, SparkContext
+from mmtfPyspark.io.mmtfReader import download_mmtf_files
+from mmtfPyspark.interactions import InteractionFilter
+from mmtfPyspark.interactions import InteractionFingerprinter
+
+class PolymerInteractionFingerprintTest(unittest.TestCase):
+
+    def setUp(self):
+
+        conf = SparkConf().setMaster(
+            "local[*]").setAppName('PolymerInteractionFingerprint')
+        self.sc = SparkContext(conf=conf)
+        # download structure 1OHR
+        self.pdb = download_mmtf_files(['1OHR'], self.sc)
+
+
+
+    def test1(self):
+
+        interactionFilter = InteractionFilter(distanceCutoff=4.0, minInteractions=10)
+        interactions = InteractionFingerprinter.get_polymer_interactions(self.pdb, interactionFilter)
+        self.assertTrue(interactions.count() == 2)
+
+    def test2(self):
+
+        interactionFilter = InteractionFilter(distanceCutoff=3.5, minInteractions=1)
+        interactionFilter.set_query_groups(True, ['ASP'])
+        interactionFilter.set_query_atom_names(True, ['OD1', 'OD2'])
+        interactionFilter.set_target_groups(True, ['ARG'])
+        interactionFilter.set_target_atom_names(True, ['NH1', 'NH2'])
+
+        interactions = InteractionFingerprinter.get_polymer_interactions(self.pdb, interactionFilter)
+        self.assertTrue(interactions.count() == 2)
+
+    def tearDown(self):
+        self.sc.stop()
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
