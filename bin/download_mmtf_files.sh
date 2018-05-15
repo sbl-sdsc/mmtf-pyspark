7b6c014b850fe3cce657880b91fb083516550108
marshuang80 <marshuang80@gmail.com>
2018-05-14 17:09:30 -0700

    [UPDATE] taking in -o flag for output directory

--- bin/download_mmtf_files.sh
+++ bin/download_mmtf_files.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
-cd /tmp
+
+OUTPUT=''
+
+# Taking in directory specified for installation
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help                show brief help"
+                        echo "-o, --output-dir=DIR      specify a directory for installation"
+                        exit 0
+                        ;;
+                -o)
+                        shift
+                        if test $# -gt 0; then
+                                export OUTPUT=$1
+                        fi
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+if [[ $OUTPUT = '' ]]; then
+  OUTPUT=$HOME
+  echo "Download location not specified, using default home directory: $OUTPUT"
+fi 
+
+if [ ! -d "$OUTPUT" ]; then
+  OUTPUT=$HOME
+  echo 'Directory does not exist, using default home directory: ' $OUTPUT 
+fi
+
+
+cd $OUTPUT
 
 # download mmtf full and reduced file
 curl -O http://mmtf.rcsb.org/v1.0/hadoopfiles/full.tar
@@ -8,8 +45,8 @@ curl -O http://mmtf.rcsb.org/v1.0/hadoopfiles/reduced.tar
 tar -xvf reduced.tar
 
 # Set environmental variables
-echo "export MMTF_REDUCED=/tmp/reduced/" >> ~/.bashrc
-echo "export MMTF_FULL=/tmp/full/" >> ~/.bashrc
+echo "export MMTF_REDUCED=$OUTPUT/reduced/" >> ~/.bashrc
+echo "export MMTF_FULL=$OUTPUT/full/" >> ~/.bashrc
 
 cd ~
 source ~/.bashrc
--- bin/install_spark.sh
+++ bin/install_spark.sh
@@ -2,17 +2,51 @@
 # define hadoop and spark version
 SPARK_VERSION='2.3.0'
 HADOOP_VERSION='2.7'
+OUTPUT=''
+
+# Taking in directory specified for installation
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo " "
+                        echo "options:"
+                        echo "-h, --help                show brief help"
+                        echo "-o, --output-dir=DIR      specify a directory for installation"
+                        exit 0
+                        ;;
+                -o)
+                        shift
+                        if test $# -gt 0; then
+                                export OUTPUT=$1
+                        fi
+                        shift
+                        ;;
+                *)
+                        break
+                        ;;
+        esac
+done
+
+if [[ $OUTPUT = '' ]]; then
+  OUTPUT=$HOME
+  echo "Installation directory not specified, using default home directory: $OUTPUT"
+fi 
+
+if [ ! -d "$OUTPUT" ]; then
+  OUTPUT=$HOME
+  echo 'Directory does not exist, using default home directory: ' $OUTPUT 
+fi
 
 # download spark from mirror image
-curl http://mirror.cogentco.com/pub/apache/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark.tgz
+curl http://mirror.cogentco.com/pub/apache/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output $OUTPUT/spark.tgz
 
 # unzip spark
-cd /tmp && tar -xvzf /tmp/spark.tgz
+cd $OUTPUT && tar -xvzf $OUTPUT/spark.tgz
 
 # set spark home and pyspark paths
-echo "export SPARK_HOME=/tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION" >> ~/.bashrc
+echo "export SPARK_HOME=$OUTPUT/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION" >> ~/.bashrc
 echo "export PATH=$SPARK_HOME/bin:$PATH" >> ~/.bashrc
 echo "export PYTHONPATH=$SPARK_HOME/python:$PYTHONPATH" >> ~/.bashrc
 
-cd ~
+#cd ~
 source ~/.bashrc
