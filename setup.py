dbb4e0a2e5661787b0e0c98ad8d6448a99e9bc4d
Peter Rose <pwrose@ucsd.edu>
2018-07-26 12:33:09 -0700

    Added xlrd dependency, closes #111.

--- setup.py
+++ setup.py
@@ -26,7 +26,8 @@ mmtfPyspark_dependencies = ['pyspark==2.3.1',
                             'scipy>=1.1.0',
                             'scikit-learn>=0.19.0',
                             'py4j==0.10.7',
-                            'pyarrow==0.9.0'
+                            'pyarrow==0.9.0',
+                            'xlrd==1.1.0'
                             ]
 
 LONG_DESCRIPTION = """
