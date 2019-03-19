96cc62e4ed949422990c93300a92afc5e6541fe9
Peter Rose <pwrose@ucsd.edu>
2019-03-18 23:07:16 -0700

    try using default folder

--- vice/entry.sh
+++ vice/entry.sh
@@ -3,13 +3,15 @@
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "anonymous", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
 # Download MMTF Hadoop Sequence File: full.tar
-path=/iplant/home/pwrose/MMTF_Files/full.tar
-iget -t KcGKzCIXviJRhxR $path
+# try to copy using default folder option
+#path=/iplant/home/pwrose/MMTF_Files/full.tar
+#iget -t KcGKzCIXviJRhxR $path
+cd $HOME/vice/MMTF_Files
 tar -xvf full.tar
 rm full.tar
 
-# The full directry ends up in $HOME/vice, move it back to $HOME
-mv $HOME/vice/full $HOME/
+# The full directory to $HOME
+mv $HOME/vice/MMTF_Files/full $HOME/
 
 # Move mmtf-pyspark with notebooks to default location
 cd $HOME/vice
@@ -20,4 +22,4 @@ mv $HOME/*.ipynb $HOME/vice/
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
-exec jupyter lab --no-browser GettingStarted.ipynb
+exec jupyter lab --no-browser 
