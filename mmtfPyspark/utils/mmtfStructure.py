eaeec74e507c374e92e266430494293739bf6ccf
Peter Rose <pwrose@ucsd.edu>
2019-05-04 15:56:33 -0700

    refactored code

--- mmtfPyspark/utils/mmtfStructure.py
+++ mmtfPyspark/utils/mmtfStructure.py
@@ -50,12 +50,17 @@ class MmtfStructure(object):
         self.num_chains = mmtfDecoder.get_value(input_data, 'numChains', required=True)
         self.num_models = mmtfDecoder.get_value(input_data, 'numModels', required=True)
         self.group_list = mmtfDecoder.get_value(input_data, 'groupList', required=True)
-        self.bond_atom_list = mmtfDecoder.decode(input_data, 'bondAtomList')
-        self.bond_order_list = mmtfDecoder.decode(input_data, 'bondOrderList')
+        self._bond_atom_list = None
+        #self.bond_atom_list = mmtfDecoder.decode(input_data, 'bondAtomList')
+        self._bond_order_list = None
+        #self.bond_order_list = mmtfDecoder.decode(input_data, 'bondOrderList')
         self.bondResonanceList = None  # TODO
-        self.x_coord_list = mmtfDecoder.decode(input_data, 'xCoordList', required=True)
-        self.y_coord_list = mmtfDecoder.decode(input_data, 'yCoordList', required=True)
-        self.z_coord_list = mmtfDecoder.decode(input_data, 'zCoordList', required=True)
+        self._x_coord_list = None
+        self._y_coord_list = None
+        self._z_coord_list = None
+        #self.x_coord_list = mmtfDecoder.decode(input_data, 'xCoordList', required=True)
+        #self.y_coord_list = mmtfDecoder.decode(input_data, 'yCoordList', required=True)
+        #self.z_coord_list = mmtfDecoder.decode(input_data, 'zCoordList', required=True)
         self._b_factor_list = None
         self._atom_id_list = None
         self._alt_loc_list = None
@@ -65,15 +70,71 @@ class MmtfStructure(object):
         #self.atom_id_list = mmtfDecoder.decode(input_data, 'atomIdList')
         #self.alt_loc_list = mmtfDecoder.decode(input_data, 'altLocList')
         #self.occupancy_list = mmtfDecoder.decode(input_data, 'occupancyList')
-        self.group_id_list = mmtfDecoder.decode(input_data, 'groupIdList', required=True)
-        self.group_type_list = mmtfDecoder.decode(input_data, 'groupTypeList', required=True)
+        self._group_id_list = None
+        #self.group_id_list = mmtfDecoder.decode(input_data, 'groupIdList', required=True)
+        self._group_type_list = None
+        #self.group_type_list = mmtfDecoder.decode(input_data, 'groupTypeList', required=True)
         #self.sec_struct_list = mmtfDecoder.decode(input_data, 'secStructList')
-        self.ins_code_list = mmtfDecoder.decode(input_data, 'insCodeList')
-        self.sequence_index_list = mmtfDecoder.decode(input_data, 'sequenceIndexList')
-        self.chain_id_list = mmtfDecoder.decode(input_data, 'chainIdList', required=True)
-        self.chain_name_list = mmtfDecoder.decode(input_data, 'chainNameList')
+        self._ins_code_list = None
+        #self.ins_code_list = mmtfDecoder.decode(input_data, 'insCodeList')
+        self._sequence_index_list = None
+        #self.sequence_index_list = mmtfDecoder.decode(input_data, 'sequenceIndexList')
+        self._chain_id_list = None
+        #self.chain_id_list = mmtfDecoder.decode(input_data, 'chainIdList', required=True)
+        self._chain_name_list = None
+        #self.chain_name_list = mmtfDecoder.decode(input_data, 'chainNameList')
         self.groups_per_chain = mmtfDecoder.get_value(input_data, 'groupsPerChain', required=True)
         self.chains_per_model = mmtfDecoder.get_value(input_data, 'chainsPerModel', required=True)
+
+    @property
+    def bond_atom_list(self):
+        if self._bond_atom_list is not None:
+            return self._bond_atom_list
+        elif 'bondAtomList' in self.input_data:
+            self._bond_atom_list = mmtfDecoder.decode(self.input_data, 'bondAtomList')
+            return self._bond_atom_list
+        else:
+            return None
+
+    @property
+    def bond_order_list(self):
+        if self._bond_order_list is not None:
+            return self._bond_order_list
+        elif 'bondOrderList' in self.input_data:
+            self._bond_order_list = mmtfDecoder.decode(self.input_data, 'bondOrderList')
+            return self._bond_order_list
+        else:
+            return None
+
+    @property
+    def x_coord_list(self):
+        if self._x_coord_list is not None:
+            return self._x_coord_list
+        elif 'xCoordList' in self.input_data:
+            self._x_coord_list = mmtfDecoder.decode(self.input_data, 'xCoordList', required=True)
+            return self._x_coord_list
+        else:
+            return None
+
+    @property
+    def y_coord_list(self):
+        if self._y_coord_list is not None:
+            return self._y_coord_list
+        elif 'yCoordList' in self.input_data:
+            self._y_coord_list = mmtfDecoder.decode(self.input_data, 'yCoordList', required=True)
+            return self._y_coord_list
+        else:
+            return None
+
+    @property
+    def z_coord_list(self):
+        if self._z_coord_list is not None:
+            return self._z_coord_list
+        elif 'zCoordList' in self.input_data:
+            self._z_coord_list = mmtfDecoder.decode(self.input_data, 'zCoordList', required=True)
+            return self._z_coord_list
+        else:
+            return None
 
     @property
     def b_factor_list(self):
@@ -116,6 +177,26 @@ class MmtfStructure(object):
             return None
 
     @property
+    def group_id_list(self):
+        if self._group_id_list is not None:
+            return self._group_id_list
+        elif 'groupIdList' in self.input_data:
+            self._group_id_list = mmtfDecoder.decode(self.input_data, 'groupIdList', required=True)
+            return self._group_id_list
+        else:
+            return None
+
+    @property
+    def group_type_list(self):
+        if self._group_type_list is not None:
+            return self._group_type_list
+        elif 'groupTypeList' in self.input_data:
+            self._group_type_list = mmtfDecoder.decode(self.input_data, 'groupTypeList', required=True)
+            return self._group_type_list
+        else:
+            return None
+
+    @property
     def sec_struct_list(self):
         if self._sec_struct_list is not None:
             return self._sec_struct_list
@@ -124,3 +205,44 @@ class MmtfStructure(object):
             return self._sec_struct_list
         else:
             return None
+
+    @property
+    def ins_code_list(self):
+        if self._ins_code_list is not None:
+            return self._ins_code_list
+        elif 'insCodeList' in self.input_data:
+            self._ins_code_list = mmtfDecoder.decode(self.input_data, 'insCodeList')
+            return self._ins_code_list
+        else:
+            return None
+
+    @property
+    def sequence_index_list(self):
+        if self._sequence_index_list is not None:
+            return self._sequence_index_list
+        elif 'sequenceIndexList' in self.input_data:
+            self._sequence_index_list = mmtfDecoder.decode(self.input_data, 'sequenceIndexList')
+            return self._sequence_index_list
+        else:
+            return None
+
+    @property
+    def chain_id_list(self):
+        if self._chain_id_list is not None:
+            return self._chain_id_list
+        elif 'chainIdList' in self.input_data:
+            self._chain_id_list = mmtfDecoder.decode(self.input_data, 'chainIdList', required=True)
+            return self._chain_id_list
+        else:
+            return None
+
+    @property
+    def chain_name_list(self):
+        if self._chain_name_list is not None:
+            return self._chain_name_list
+        elif 'chainNameList' in self.input_data:
+            self._chain_name_list = mmtfDecoder.decode(self.input_data, 'chainNameList')
+            return self._chain_name_list
+        else:
+            return None
+
