7d76c1e7706996365c2a9661cb1a152e56664c93
Peter Rose <pwrose@ucsd.edu>
2019-05-06 15:54:04 -0700

    simplify spark initialization

--- mmtfPyspark/utils/__init__.py
+++ mmtfPyspark/utils/__init__.py
@@ -1,6 +1,5 @@
 from .columnarStructure import ColumnarStructure
 from .columnarStructureX import ColumnarStructureX
-from .col_structure import ColStructure
 from .mmtfStructure import MmtfStructure
 from .dsspSecondaryStructure import DsspSecondaryStructure
 from .distanceBox import DistanceBox
