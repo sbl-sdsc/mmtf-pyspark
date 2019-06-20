9fe320f8d09eaf6fa2c0360c40e65d92c03f23e2
Peter Rose <pwrose@ucsd.edu>
2019-06-19 22:30:49 -0700

    [WIP] new decoder

--- mmtfPyspark/utils/mmtfStructure.py
+++ mmtfPyspark/utils/mmtfStructure.py
@@ -21,8 +21,8 @@ class MmtfStructure(object):
         """Decodes a msgpack unpacked data to mmtf structure"""
         self.input_data = input_data
 
-        #self.decoder = Codec()
-        self.decoder = mmtfCodec
+        self.decoder = Codec()
+        #self.decoder = mmtfCodec
 
         self.mmtf_version = mmtfDecoder.get_value(input_data, 'mmtfVersion', required=True)
         self.mmtf_producer = mmtfDecoder.get_value(input_data, 'mmtfProducer', required=True)
