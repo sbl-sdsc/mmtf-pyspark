f57fffaa584391680759f266893cc921a52d9288
Peter Rose <pwrose@ucsd.edu>
2019-06-22 13:41:15 -0700

    [WIP] new write method

---
+++ mmtfPyspark/tests/io/test_MmtfWriter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+'''
+readSequenceFromListTest.py: Example reading a list of PDB IDs from a local MMTF Hadoop sequence \
+file into a tubleRDD.
+
+Authorship information:
+__author__ = "Mars (Shih-Cheng) Huang"
+__maintainer__ = "Mars (Shih-Cheng) Huang"
+__email__ = "marshuang80@gmail.com:
+__status__ = "Warning"
+'''
+
+import unittest
+import tempfile
+from pyspark.sql import SparkSession
+from mmtfPyspark.io import mmtfReader
+from mmtfPyspark.io import mmtfWriter
+
+
+class WriteSequenceFileTest(unittest.TestCase):
+
+    def setUp(self):
+        self.spark = SparkSession.builder.appName("write_sequence_file").getOrCreate()
+
+    def test_mmtf(self):
+        path = '../../../resources/files/'
+        pdb = mmtfReader.read_mmtf_files(path)
+        self.assertTrue(pdb.count() == 3)
+        tmp_path = tempfile.mkdtemp()
+        mmtfWriter.write_mmtf_files(tmp_path, pdb)
+        pdb = mmtfReader.read_mmtf_files(tmp_path)
+        self.assertTrue(pdb.count() == 3)
+
+    def tearDown(self):
+        self.spark.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
