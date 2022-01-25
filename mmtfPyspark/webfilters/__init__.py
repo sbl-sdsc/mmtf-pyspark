bd226f065698c1f030d6ad56f874db2ceb3c1860
Peter Rose <pwrose.ucsd@gmail.com>
2022-01-24 23:10:16 -0800

    remove obsolete methods

--- mmtfPyspark/webfilters/__init__.py
+++ mmtfPyspark/webfilters/__init__.py
@@ -2,7 +2,5 @@ from .pisces import Pisces
 from .advancedQuery import AdvancedQuery
 from .chemicalStructureQuery import ChemicalStructureQuery
 from .sequenceSimilarity import SequenceSimilarity
-from .wildTypeQuery import WildTypeQuery
 from .customReportQuery import CustomReportQuery
-from .blastCluster import BlastCluster
 from .pdbjMineSearch import PdbjMineSearch
